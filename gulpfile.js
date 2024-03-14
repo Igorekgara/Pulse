0a505bdcbabcc8d357429ad96b802c57502dfaa1
Igor <f1n_xek@bk.ru>
2024-03-14 23:39:43 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+const gulp        = require('gulp'); //говорим gulp об использовании того или иного пакета
+const browserSync = require('browser-sync');
+const sass = require('gulp-sass')(require('sass'));
+const rename = require("gulp-rename");
+//const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+
+// Static server
+gulp.task('server', function() { //указываем задачу для gulp, live сервер
+    browserSync.init({
+        server: {
+            baseDir: "src"
+        }
+    });
+});
+
+gulp.task('styles', function() { //компиляция кода sass
+    return gulp.src("src/sass/*.+(scss|sass)")
+            .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+            .pipe(rename({
+                prefix: "",
+                suffix: ".min",
+              }))
+            //.pipe(autoprefixer({
+                //browsers: ['2 last versions'],
+                //cascade: false,
+            //}))
+            .pipe(cleanCSS({compatibility: 'ie8'}))
+            .pipe(gulp.dest("src/css"))
+            .pipe(browserSync.stream());
+});
+
+gulp.task('watch', function() { //прослеживание обновлений файлов html и sass
+    gulp.watch("src/sass/*.+(scss|sass)", gulp.parallel("styles"))
+    gulp.watch("src/*.html").on("change", browserSync.reload);
+});
+
+gulp.task('default', gulp.parallel('watch', 'server', 'styles')); //объединяем и запускаем задачи
