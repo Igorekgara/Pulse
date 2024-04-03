24295c3bcb45c907a3b06d523a15f0d1010a998c
Igor <f1n_xek@bk.ru>
2024-04-04 01:04:23 +0300

    Created script.js file. Changed file fonts.css

---
+++ src/js/script.js
@@ -0,0 +1,50 @@
+var name = 'Igor';
+let number = 7;
+const pi = 3.14;
+
+number = 5;
+
+let leftBorderWidth = 200;
+
+console.log(number);
+
+//let answer = confirm("Вам есть 18?");
+//console.log(answer);
+
+//let answer = prompt("Вам есть 18?", "");
+//console.log(answer);
+
+//let isChecked = false,
+  //  isClose = false;
+//console.log(isChecked && isClose);
+
+//console.log(isChecked || isClose);
+
+//let answer = confirm("Вам есть 18?");
+//if (answer) {
+//    console.log('Проходите')
+//} else {
+//    console.log('Уходите')
+//}
+
+/* const num = 50;
+
+if (num < 49) {
+    console.log('Неверно')
+} else if (num > 100) {
+    console.log('Много')
+} else {
+    console.log('Верно')
+} */
+
+for (let i = 1; i < 8; i++) {
+    console.log(i)
+}
+
+function logging(a, b) {
+    console.log ( a + b )
+}
+
+logging(3, 5);
+
+logging(6, 8);
